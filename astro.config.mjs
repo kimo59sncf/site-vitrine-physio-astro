506fe1482f7518bb7232a5b5fbde9a6abc3ac4f8
kimo59 <7810805f@commun.ad.sncf.fr>
2025-12-31 12:56:58 +0100

    Fix: Corriger la base URL en production pour les images

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,17 +3,17 @@ import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import node from "@astrojs/node";
 
-const isDevBranch = process.env.GITHUB_REF === 'refs/heads/dev';
+const isDev = process.env.NODE_ENV === 'development' || process.env.GITHUB_REF === 'refs/heads/dev';
 
 export default defineConfig({
   integrations: [tailwind({
     config: "./tailwind.config.mjs"
   }), react()],
-  adapter: isDevBranch ? undefined : node({ mode: 'standalone' }),
-  site: isDevBranch ? 'https://kimo59sncf.github.io' : 'https://physiokbnyon.ch',
-  base: isDevBranch ? '/site-vitrine-physio-astro' : '/',
-  output: isDevBranch ? 'static' : 'server',
-  ...(isDevBranch ? {} : {
+  adapter: isDev ? undefined : node({ mode: 'standalone' }),
+  site: isDev ? 'https://kimo59sncf.github.io' : 'https://www.physiokbnyon.ch',
+  base: isDev ? '/site-vitrine-physio-astro' : '',
+  output: isDev ? 'static' : 'server',
+  ...(isDev ? {} : {
     static: {
       host: '0.0.0.0',
       port: 4327,
